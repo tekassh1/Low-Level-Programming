348d654223160606e2e5aed9d1e1ed97d696302e
geeks <geekslovee@gmail.com>
2023-10-24 20:11:37 +0300

    3'rd task completed.

---
+++ seminar4/task3/asm_headers.h
@@ -0,0 +1,7 @@
+#ifndef ASM_HEADERS
+#define ASM_HEADERS
+
+void print_string(char* str);
+void world();
+
+#endif
---
+++ seminar4/task3/hello.c
@@ -0,0 +1,9 @@
+// extern void print_string(char* str);
+// extern void world();
+
+#include "asm_headers.h"
+
+int main() {
+    print_string("hello");
+    world();
+}
