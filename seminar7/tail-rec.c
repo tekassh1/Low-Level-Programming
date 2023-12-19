d825f06d8a45485b85a1b79b6316da3c478bc3b3
geeks <geekslovee@gmail.com>
2023-12-20 02:15:08 +0300

    seminar8 finished.

---
+++ seminar7/tail-rec.c
@@ -0,0 +1,40 @@
+/* Task: avoid stack overflow while running list_length*/
+
+#include <inttypes.h>
+#include <malloc.h>
+#include <stddef.h>
+#include <stdio.h>
+
+void print_size(size_t i) { printf("%zu" , i); }
+
+struct list {
+  int64_t elem;
+  struct list *next;
+};
+
+struct list *c(int64_t head, struct list *tail) {
+  struct list *h = (struct list *)malloc(sizeof(struct list));
+  h->elem = head;
+  h->next = tail;
+  return h;
+}
+
+size_t list_length(struct list const *l) {
+  // Stack overflow. Solution - "while" or -O3 compilation
+  if (!l)
+    return 0;
+  return 1 + list_length(l->next);
+}
+
+int main(int argc, char **argv) {
+  const size_t len = 1024 * 1024;
+
+  struct list *lst = NULL;
+
+  for( size_t i = 0; i < len; i++) {
+    lst = c(i, lst);
+  }
+
+  print_size(list_length(lst));
+  return 0;
+}
